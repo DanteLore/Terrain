480fa22301d64a7601e040e2c86b9bca31edafc6
DanteLore <dan.taylor@thetrainline.com>
2021-04-13 11:07:39 +0100

    Noise settings - episode 2

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,17 +4,38 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    public int mapWidth;
-    public int mapHeight;
-    public float noiseScale;
-    public bool autoUpdate;
+    public int mapWidth = 100;
+    public int mapHeight = 100;
+    public float noiseScale = 10f;
+    public bool autoUpdate = true;
+    public int octaves = 3;
+    [Range(0, 1)]
+    public float persistence = 0.5f;
+    public float lacunarity = 2f;
+    public int seed;
+    public Vector2 offset;
 
     public void GenerateMap() 
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistence, lacunarity, offset);
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
 
         display.DrawNoiseMap(noiseMap);
+    }
+
+    void OnValidate()
+    {
+        if(mapWidth < 1)
+            mapWidth = 1;
+
+        if(mapHeight < 1)
+            mapHeight = 1;
+
+        if(lacunarity < 1)
+            lacunarity = 1;
+
+        if(octaves <= 0)
+            octaves = 1;
     }
 }
