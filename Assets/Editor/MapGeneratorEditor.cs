95a60c6ebdabc83fd1719d2213f8a11f30e194a7
DanteLore <dan.taylor@thetrainline.com>
2021-04-13 10:17:46 +0100

    Initial Commit - episode 2

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        if(DrawDefaultInspector() && mapGen.autoUpdate)
+            mapGen.GenerateMap();
+
+        if(GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Terrain Generator
+
+This game was made following this tutorial: https://www.youtube.com/watch?v=WP-Bm65Q-1Y
+
+There's nothing special to see here - YET! :)
