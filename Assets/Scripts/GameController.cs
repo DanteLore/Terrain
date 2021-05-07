64fdcf549089ec58c1a1d3070c417230e345fede
DanteLore <dan.taylor@thetrainline.com>
2021-05-07 14:11:09 +0100

    Added a main menu and quit button

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,31 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class GameController : MonoBehaviour
 {
+    public Canvas mainMenuCanvas;
     public Text worldPosText;
-
     public Text headingText;
-
     public TerrainGenerator terrainGenerator; 
+
+    RigidbodyFirstPersonController playerController;
+    MouseLook mouseLook;
 
     public GameObject player;
 
-    // Start is called before the first frame update
+    public GameObject menuObject;
+    public GameObject hudObject;
+
+    private bool inMenu = false;
+
     void Start()
     {
-        
+        playerController = player.GetComponent<RigidbodyFirstPersonController>();
+        mouseLook = playerController.mouseLook;
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            inMenu = !inMenu;
+            mouseLook.SetCursorLock(!inMenu);
+            mouseLook.UpdateCursorLock();
+            playerController.enabled = !inMenu;
+            menuObject.SetActive(inMenu);
+            hudObject.SetActive(!inMenu);
+        }
+
+        // Emergency use only :)
+        if(Input.GetKeyDown(KeyCode.Delete))
+        {
+            Debug.Break();
+        }
+            
+        UpdateHud();
+    }
+
+    private void UpdateHud()
     {
         Vector2 pos = terrainGenerator.GameToMapPos(player.transform.position);
         float heading =  player.transform.rotation.eulerAngles.y;
 
-        worldPosText.text = string.Format("X: {0:0.0} Y: {1:0.0}", pos.x, pos.y);
+        worldPosText.text = string.Format("X: {0:0.0} Z: {1:0.0}", pos.x, pos.y);
         headingText.text = string.Format("{0:0}°", heading);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Bye bye!");
+        Application.Quit();
     }
 }
